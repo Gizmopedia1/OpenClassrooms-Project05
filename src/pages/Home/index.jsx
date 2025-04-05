b6307120a0fa54e96b21c31570937817c6095894
Joel <jbisselele@gmail.com>
2025-04-05 22:07:48 +0200

    3rd commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import './index.css'
 import Nav from '../../components/Nav/Nav'
 import Banner from '../../components/Banner/Banner'
+import Footer from '../../components/Footer/Footer'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
       <Nav />
       <Banner />
+      <Footer />
       </div>
     </>
   )
