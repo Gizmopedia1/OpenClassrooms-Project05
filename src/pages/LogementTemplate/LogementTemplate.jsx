5d1c01dbbe6b885cb63dd99e447946562e31c0d0
Joel <jbisselele@gmail.com>
2025-04-16 18:09:50 +0200

    Responsive logement

--- src/pages/LogementTemplate/LogementTemplate.jsx
+++ src/pages/LogementTemplate/LogementTemplate.jsx
@@ -1,5 +1,4 @@
 import './LogementTemplate.scss'
-import { useEffect, useState } from "react";
 import Slider from '../../components/Slider/Slider.jsx'
 import LogementTitle from '../../components/LogementTitle/LogementTitle.jsx'
 import Host from '../../components/Host/Host.jsx'
@@ -21,34 +20,22 @@ function LogementTemplate() {
     }
     const slides = item.pictures
 
-    const useIsResponsive = () => {
-      const [isResponsive, setIsResponsive] = useState(false);
-    
-      useEffect(() => {
-        const mediaQuery = window.matchMedia("(max-width: 768px)");
-    
-        const handleChange = () => setIsResponsive(mediaQuery.matches);
-        
-        handleChange(); // initial check
-        mediaQuery.addEventListener("change", handleChange);
-    
-        return () => mediaQuery.removeEventListener("change", handleChange);
-      }, []);
-    
-      return isResponsive;
-    };
-    
-    
     return (
         <div>
           <Slider slides={slides}/>
-          <div className='title-host'>
-            <LogementTitle title={item.title} location={item.location} />
-            <Host hostName={item.host.name} hostPicture={item.host.picture} />
-          </div>
-          <div className='tags-stars'>
-            <Tags tagList={item.tags} />
-            <Rating rating={item.rating} />
+          <div className='grid-details'>
+            <div className='logement-title'>
+              <LogementTitle title={item.title} location={item.location} />
+            </div>
+            <div className='host-card'>
+              <Host hostName={item.host.name} hostPicture={item.host.picture} />
+            </div>
+            <div className='tags-list'>
+              <Tags tagList={item.tags} />
+            </div>
+            <div className='rating'>
+              <Rating rating={item.rating} />
+            </div>
           </div>
           <div className="two-collapse">
             <Collapse title='Description' content={item.description} />
