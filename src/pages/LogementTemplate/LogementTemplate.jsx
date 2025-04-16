030a2fa23059c5f4aeab2f3f10d4a1d756c62c21
Joel <jbisselele@gmail.com>
2025-04-16 17:29:38 +0200

    Avancée

--- src/pages/LogementTemplate/LogementTemplate.jsx
+++ src/pages/LogementTemplate/LogementTemplate.jsx
@@ -1,19 +1,44 @@
 import './LogementTemplate.scss'
+import { useEffect, useState } from "react";
 import Slider from '../../components/Slider/Slider.jsx'
 import LogementTitle from '../../components/LogementTitle/LogementTitle.jsx'
 import Host from '../../components/Host/Host.jsx'
 import Collapse from '../../components/Collapse/Collapse.jsx'
 import Tags from '../../components/Tags/Tags.jsx'
 import Rating from '../../components/Rating/Rating.jsx'
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import logements from '../../data/logements.json'
+import Error404 from '../../components/Error404/Error404.jsx'
 
 
 function LogementTemplate() {
     const { id } = useParams();
     const item = logements.find((item) => item.id === id);
+    if (item == null) {
+      return (
+        <Error404 />
+      )
+    }
     const slides = item.pictures
 
+    const useIsResponsive = () => {
+      const [isResponsive, setIsResponsive] = useState(false);
+    
+      useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 768px)");
+    
+        const handleChange = () => setIsResponsive(mediaQuery.matches);
+        
+        handleChange(); // initial check
+        mediaQuery.addEventListener("change", handleChange);
+    
+        return () => mediaQuery.removeEventListener("change", handleChange);
+      }, []);
+    
+      return isResponsive;
+    };
+    
+    
     return (
         <div>
           <Slider slides={slides}/>
