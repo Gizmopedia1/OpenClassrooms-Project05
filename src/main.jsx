bf4fadb999fe9ffce1a6e913d931e26b64ccc342
Joel <jbisselele@gmail.com>
2025-04-30 13:14:00 +0200

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './main.scss'
 import App from './pages/App/App.jsx'
+import ReactDOM from "react-dom/client";
 
- 
-createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Router basename="/OpenClassrooms-Project05">
-            <App />
-        </Router>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/OpenClassrooms-Project05">
+    <App />
+  </BrowserRouter>
+);
