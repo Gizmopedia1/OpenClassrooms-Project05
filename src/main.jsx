93a81d9804dec28ac3267ae806b87f7b2f5ae2af
Joel <jbisselele@gmail.com>
2025-04-30 13:18:31 +0200

    routeur

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './main.scss'
 import App from './pages/App/App.jsx'
-import ReactDOM from "react-dom/client";
 
+ 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/OpenClassrooms-Project05">
-    <App />
-  </BrowserRouter>
-);
+        <BrowserRouter  basename="/OpenClassrooms-Project05">
+            <App />
+        </BrowserRouter>
+)
+
+// index.js ou main.jsx
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <BrowserRouter basename="/nom-du-repo">
+//     <App />
+//   </BrowserRouter>
+// );
