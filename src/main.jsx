f6eb8b7f7a951c8216224d0cd44dffd18ea60742
Joel <jbisselele@gmail.com>
2025-05-01 16:44:18 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './pages/App/App.jsx'
 
  
 ReactDOM.createRoot(document.getElementById("root")).render(
-        <BrowserRouter basename="https://gizmopedia1.github.io/OpenClassrooms-Project05">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
 )
