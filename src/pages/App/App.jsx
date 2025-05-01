89b9fdef1adba61ad91679fda87778a869fc88ef
Joel <jbisselele@gmail.com>
2025-05-01 16:46:46 +0200

    commit

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -17,11 +17,11 @@ function App() {
     <div>
           <Nav />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/404" element={<Page404 />} />
+            <Route path="/OpenClassrooms-Project05" element={<Home />} />
+            <Route path="/OpenClassrooms-Project05/404" element={<Page404 />} />
             <Route path="*" element={<Page404 />} />
-            <Route path="/a-propos" element={<APropos />} />
-            <Route path="/details/:id" element={<LogementTemplate />} />
+            <Route path="/OpenClassrooms-Project05/a-propos" element={<APropos />} />
+            <Route path="/OpenClassrooms-Project05/details/:id" element={<LogementTemplate />} />
           </Routes>
           <Footer />
     </div>
